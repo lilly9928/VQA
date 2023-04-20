f77c2e05b4888806a8ba0fbefc8b67ab84f520a0
codingbus821 <kbs23@yonsei.ac.kr>
2023-04-20 12:33:48 +0900

    update util, dataloader for VizWiz

---
+++ data_loader_for_VizWiz.py
@@ -0,0 +1,126 @@
+import json
+import os
+import numpy as np
+import torch
+import torch.utils.data as data
+import torchvision.transforms as transforms
+from PIL import Image
+from matplotlib import pyplot as plt
+
+from util import text_helper
+
+
+class VqaDataset(data.Dataset):
+
+    def __init__(self, input_dir, input_vqa, max_qst_length=30, max_num_ans=10, transform=None):
+        self.input_dir = input_dir
+        self.qst_vocab = text_helper.VocabDict(input_dir + '/vocab_questions.txt')
+        self.ans_vocab = text_helper.VocabDict(input_dir + '/vocab_answers.txt')
+        with open(input_dir + input_vqa, encoding='UTF8') as f:
+            self.vqa = json.load(f)
+        # self.vqa_length = len(self.vqa)
+
+        # self.vqa = np.load(input_dir+'/'+input_vqa,allow_pickle = True)
+        # self.max_qst_length = max_qst_length
+        # self.max_num_ans = max_num_ans
+        # self.load_ans = ('valid_answers' in self.vqa[0]) and (self.vqa[0]['valid_answers'] is not None)
+        # self.transform = transform
+
+    def __getitem__(self, idx):
+
+        vqa = self.vqa
+        qst_vocab = self.qst_vocab
+        ans_vocab = self.ans_vocab
+        max_qst_length = self.max_qst_length
+        max_num_ans = self.max_num_ans
+        transform = self.transform
+        load_ans = self.load_ans
+
+        image = vqa[idx]['image_path']
+        image = Image.open(image).convert('RGB')
+        qst2idc = np.array([qst_vocab.word2idx('<pad>')] * max_qst_length)  # padded with '<pad>' in 'ans_vocab'
+        qst2idc[:len(vqa[idx]['question_tokens'])] = [qst_vocab.word2idx(w) for w in vqa[idx]['question_tokens']]
+        sample = {'image': image, 'question': qst2idc}
+
+        if load_ans:
+            ans2idc = [ans_vocab.word2idx(w) for w in vqa[idx]['valid_answers']]
+            ans2idx = np.random.choice(ans2idc)
+            sample['answer_label'] = ans2idx         # for training
+
+            mul2idc = list([-1] * max_num_ans)       # padded with -1 (no meaning) not used in 'ans_vocab'
+            mul2idc[:len(ans2idc)] = ans2idc         # our model should not predict -1
+            sample['answer_multi_choice'] = mul2idc  # for evaluation metric of 'multiple choice'
+
+        if transform:
+            sample['image'] = transform(sample['image'])
+
+        return sample
+
+    def __len__(self):
+
+        return len(self.vqa)
+
+
+def get_loader(input_dir, input_vqa_train, input_vqa_valid, max_qst_length, max_num_ans, batch_size, num_workers):
+
+    # transform = {
+    #     phase: transforms.Compose([transforms.ToTensor(),
+    #                                transforms.Normalize((0.485, 0.456, 0.406),
+    #                                                     (0.229, 0.224, 0.225))])
+    #     for phase in ['train', 'valid',]}
+
+    vqa_dataset = {
+        'train': VqaDataset(
+            input_dir=input_dir,
+            input_vqa=input_vqa_train,
+            max_qst_length=max_qst_length,
+            max_num_ans=max_num_ans,
+            ),
+        'valid': VqaDataset(
+            input_dir=input_dir,
+            input_vqa=input_vqa_valid,
+            max_qst_length=max_qst_length,
+            max_num_ans=max_num_ans,
+            ),
+
+    }
+
+    data_loader = {
+        phase: torch.utils.data.DataLoader(
+            dataset=vqa_dataset[phase],
+            batch_size=batch_size,
+            shuffle=True,
+            num_workers=num_workers)
+        for phase in ['train', 'valid']}
+
+    return data_loader
+
+if __name__ == "__main__":
+
+    input_dir = 'C:/Users/andlabkbs/Desktop/dataset/Visual_Question_Answering/'
+    log_dir = './logs'
+    model_dir='./models'
+    max_qst_length = 30
+    max_num_ans =10
+    embed_size=64
+    word_embed_size=300
+    num_layers=2
+    hidden_size=16
+    learning_rate = 0.001
+    step_size = 10
+    gamma = 0.1
+    num_epochs=30
+    batch_size = 64
+    num_workers = 4
+    save_step=1
+
+    data = get_loader(
+        input_dir=input_dir,
+        input_vqa_train='Annotations/foranswer/train.json',
+        input_vqa_valid='Annotations/foranswer/val.json',
+        max_qst_length=max_qst_length,
+        max_num_ans=max_num_ans,
+        batch_size=batch_size,
+        num_workers=num_workers)
+
+    print(data['train'])
